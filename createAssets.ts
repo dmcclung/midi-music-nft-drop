de9406785a222af4a003500e84851523c6cbfa18
dmcclung <dylanmcclung@gmail.com>
2021-12-04 22:04:08 -0600

    Added scribbletunes

--- createAssets.ts
+++ createAssets.ts
@@ -1,5 +1,7 @@
 import { writeFileSync } from 'fs'
 import { range } from 'lodash'
+import * as scribble from 'scribbletune'
+import text2png from 'text2png'
 
 // write 0.json, 0.png, 0.mid
 
@@ -9,7 +11,47 @@ console.dir(numNfts)
 console.log('Creating NFT assets...')
 
 numNfts.forEach(n => {
-    writeFileSync(`assets/${n}.png`, '')
-    writeFileSync(`assets/${n}.mid`, '')
-    writeFileSync(`assets/${n}.json`, '')
+
+  const png = text2png('Dm BbM Am FM BbM FM CM Gm', {
+    color: 'blue',
+    backgroundColor: 'white' 
+  })
+
+  writeFileSync(`assets/${n}.png`, png)
+
+  const clips = ['1032', '2032', '4021', '3052'].map(order =>
+    scribble.clip({
+      pattern: '[xx][xR]'.repeat(4),
+      notes: scribble.arp({
+        chords: 'Dm BbM Am FM BbM FM CM Gm',
+        count: 8,
+        order,
+      }),
+      accent: 'x-xx--xx',
+    })
+  )
+  scribble.midi([].concat(...clips), `assets/${n}.mid`)
+
+  const json = {
+    "name": `Midi Audio NFT #${n}`,
+    "symbol": "MIDI",
+    "image": `${n}.png`,
+    "properties": {
+      "files": [
+        {
+          "uri": `${n}.png`,
+          "type": "image/png"
+        }
+      ],
+      "creators": [
+        {
+          "address": "INSERT_CREATOR_WALLET_ADDRESS_HERE",
+          "share": 100
+        }
+      ]
+    }
+  }
+
+  writeFileSync(`assets/${n}.json`, JSON.stringify(json))
+
 })
