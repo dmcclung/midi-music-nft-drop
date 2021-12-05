1c0f6f1fd560b151e96afc47ec5313c24d392f71
dmcclung <dylanmcclung@gmail.com>
2021-12-05 01:00:08 -0600

    Changing up random pattern for midi

--- createAssets.ts
+++ createAssets.ts
@@ -3,34 +3,53 @@ import { range } from 'lodash'
 import * as scribble from 'scribbletune'
 import text2png from 'text2png'
 
-// write 0.json, 0.png, 0.mid
-
 const numNfts = range(0, 3)
 console.dir(numNfts)
 
 console.log('Creating NFT assets...')
 
+const root = 'B2';
+const scale = 'minor';
+
+const getRandomPattern = (count = 8) => {
+  let str = '[x-]R'
+  for (let i = 1; i < count; i++) {
+    str += Math.round(Math.random()) ? '[x-]R' : 'R[x-]'
+  }
+
+  return str
+}
+
 numNfts.forEach(n => {
 
-  const png = text2png('Dm BbM Am FM BbM FM CM Gm', {
+  const pattern = getRandomPattern()
+
+  const clipA = scribble.clip({
+    notes: root,
+    randomNotes: scribble.arp(
+      scribble.getChordsByProgression(root + ' ' + scale, 'ii iii')
+    ),
+    pattern,
+    subdiv: '16n',
+  })
+
+  const clipB = scribble.clip({
+    notes: root,
+    randomNotes: scribble.arp(
+      scribble.getChordsByProgression(root + ' ' + scale, 'vi v')
+    ),
+    pattern,
+    subdiv: '16n',
+  })
+
+  scribble.midi([].concat(clipA, clipA, clipA, clipB), `assets/${n}.mid`)
+
+  const png = text2png(pattern, {
     color: 'blue',
     backgroundColor: 'white' 
   })
 
   writeFileSync(`assets/${n}.png`, png)
-
-  const clips = ['1032', '2032', '4021', '3052'].map(order =>
-    scribble.clip({
-      pattern: '[xx][xR]'.repeat(4),
-      notes: scribble.arp({
-        chords: 'Dm BbM Am FM BbM FM CM Gm',
-        count: 8,
-        order,
-      }),
-      accent: 'x-xx--xx',
-    })
-  )
-  scribble.midi([].concat(...clips), `assets/${n}.mid`)
 
   const json = {
     "name": `Midi Audio NFT #${n}`,
@@ -45,7 +64,7 @@ numNfts.forEach(n => {
       ],
       "creators": [
         {
-          "address": "INSERT_CREATOR_WALLET_ADDRESS_HERE",
+          "address": "Br7YiN2PkG7JeSEreHRfzuXtU6TJUiAUswqoCoqvt2b4",
           "share": 100
         }
       ]
